c5dc4aa7c5a7d337aa8db81637902ab4091cf1df
Ben <benymng@gmail.com>
2022-05-19 00:00:35 -0600

    changed contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -21,20 +21,18 @@ export const ContactForm = () => {
           <div class="grid grid-cols-1 gap-x-16 gap-y-8 lg:grid-cols-5">
             <div class="lg:py-12 lg:col-span-2">
               <p class="max-w-xl text-lg">
-                At the same time, the fact that we are wholly owned and totally
-                independent from manufacturer and other group control gives you
-                confidence that we will only recommend what is right for you.
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                mollitia, molestiae quas vel sint commodi repudiandae
+                consequuntur voluptatum laborum numquam
               </p>
 
               <div class="mt-8">
                 <a href="" class="text-2xl font-bold text-pink-600">
                   {' '}
-                  0151 475 4450{' '}
+                  Phone Number{' '}
                 </a>
 
-                <address class="mt-2 not-italic">
-                  282 Kevin Brook, Imogeneborough, CA 58517
-                </address>
+                <address class="mt-2 not-italic">Contact Information</address>
               </div>
             </div>
 
