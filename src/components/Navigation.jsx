cd3dd1fa7c94f3db5e40d7aec5311806ba2c1f06
benymng <benymng@gmail.com>
2023-01-09 22:37:10 -0500

    changed navigation bar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -40,27 +40,11 @@ export const Navigation = () => {
               <li>
                 <a href="/blogs">Blogs</a>
               </li>
-              <li tabindex="0">
-                <a class="justify-between">
-                  About Me
-                  <svg
-                    class="fill-current"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                  </svg>
-                </a>
-                <ul class="p-2 z-50">
-                  <li>
-                    <a href="/projects">Projects</a>
-                  </li>
-                  <li>
-                    <a href="/contact">Contact</a>
-                  </li>
-                </ul>
+              <li>
+                <a href="/projects">Projects</a>
+              </li>
+              <li>
+                <a href="/contact">Contact</a>
               </li>
               <li>
                 <a href="/testimonials">Testimonials</a>
@@ -76,27 +60,11 @@ export const Navigation = () => {
             <li>
               <a href="/blogs">Blogs</a>
             </li>
-            <li tabindex="0">
-              <a>
-                About Me
-                <svg
-                  class="fill-current"
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="20"
-                  height="20"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                </svg>
-              </a>
-              <ul class="p-2 z-50">
-                <li>
-                  <a href="/projects">Projects</a>
-                </li>
-                <li>
-                  <a href="/contact">Contact</a>
-                </li>
-              </ul>
+            <li>
+              <a href="/projects">Projects</a>
+            </li>
+            <li>
+              <a href="/contact">Contact</a>
             </li>
             <li>
               <a href="/testimonials">Testimonials</a>
